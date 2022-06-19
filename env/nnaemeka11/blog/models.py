5c0e5da4bc99b0314d46e875942266c936d8d3bd
Nnaemeka Joseph <nnaemekaxi@yahoo.com>
2022-06-19 10:27:02 +0100

    task 76

---
+++ env/nnaemeka11/blog/models.py
@@ -0,0 +1,11 @@
+from tkinter import CASCADE
+from django.db import models
+from django.contrib.auth import get_user_model
+
+# Create your models here.
+class Post(models.Model):
+    Title = models.CharField((""), max_length=200)
+    Text = models.TextField((""))
+    Author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    Created_date = models.DateTimeField((""), auto_now=False, auto_now_add=False)
+    Published_date = models.DateTimeField((""), auto_now=False, auto_now_add=False)
